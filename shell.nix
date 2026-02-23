01dd14ec61bfb860a3236ffd879fc4829b6566e5
Jóhann Friðriksson <johann@tern.is>
2026-02-23 13:45:45 +0000

    Added line-profiler (kernprof) and formatted.

--- shell.nix
+++ shell.nix
@@ -1,18 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+{pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages (ps: with ps; [
-      python-gitlab
-      textual
-      click
-      python-dotenv
-      rich # For syntax highlighting, also a textual dependency
-      requests
-      pygments
-      fuzzywuzzy
-      ipython
-      platformdirs
-    ]))
+    (python3.withPackages (ps:
+      with ps; [
+        python-gitlab
+        textual
+        click
+        python-dotenv
+        rich # For syntax highlighting, also a textual dependency
+        requests
+        pygments
+        fuzzywuzzy
+        ipython
+        platformdirs
+        (line-profiler.overridePythonAttrs (oldAttrs: {
+          doCheck = false;
+          doInstallCheck = false;
+        }))
+      ]))
   ];
 }
